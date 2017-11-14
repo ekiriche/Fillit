024bffca9886e260009ce1081393ea7f1fa83c89
Evgeniy KIRICHENKO <ekiriche@e1r1p7.unit.ua>
2017-11-14 15:28:17 +0200

    asd

---
+++ ft_strlcat.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlact.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ekiriche <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/10/25 14:26:40 by ekiriche          #+#    #+#             */
+/*   Updated: 2017/11/07 15:43:36 by ekiriche         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	s1_end;
+
+	i = 0;
+	if (size == 0)
+		return (ft_strlen(dst));
+	while (dst[i] && i < size)
+		i++;
+	s1_end = i;
+	while (src[i - s1_end] && i < size - 1)
+	{
+		dst[i] = src[i - s1_end];
+		i++;
+	}
+	if (s1_end < size)
+		dst[i] = '\0';
+	return (s1_end + ft_strlen(src));
+}
