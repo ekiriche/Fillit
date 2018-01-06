d915e7f23252f82d8b5dd66f17b0d004e01e5495
Evgeniy KIRICHENKO <ekiriche@e3r3p1.unit.ua>
2018-01-06 14:37:49 +0200

    asd

--- gnl/mygnl/get_next_line.c
+++ gnl/mygnl/get_next_line.c
@@ -1,7 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ekiriche <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/01/06 10:33:38 by ekiriche          #+#    #+#             */
+/*   Updated: 2018/01/06 14:35:25 by ekiriche         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 #include "libft/libft.h"
 
-static int	find_endl(char *str)
+static int			find_endl(char *str)
 {
 	int	len;
 
@@ -9,73 +21,59 @@ static int	find_endl(char *str)
 	while (str[len])
 	{
 		if (str[len] == '\n')
-			break ;
-		else
-			len++;
+		{
+			str[len] = '\0';
+			return (len);
+		}
+		len++;
 	}
-	if (len == ft_strlen(str))
+	return (-42);
+}
+
+static int			write_in_line(char **buff, char **line, char **temp)
+{
+	int				len;
+
+	if (*buff)
+		*buff = ft_strjoin(*buff, *temp);
+	else
+		*buff = ft_strdup(*temp);
+	if ((len = find_endl(*buff)) != -42)
+	{
+		*line = ft_strdup(*buff);
+		*buff = ft_strdup(*buff + len + 1);
+		return (1);
+	}
+	return (0);
+}
+
+int					get_next_line(int const fd, char **line)
+{
+	static char		*buff[420];
+	int				ret;
+	int				n_found;
+	char			*temp;
+
+	temp = ft_strnew(BUFF_SIZE);
+	if (BUFF_SIZE < 0 || fd < 0 || !line || (ret = read(fd, temp, 0)) < 0)
 		return (-1);
-	return (len);
-}
-
-static char	*buff_plus_temp(char *buff, char *temp)
-{
-	size_t	i;
-	size_t	j;
-	char	*ans;
-
-	i = 0;
-	if (buff)
-		i = ft_strlen(buff);
-	j = 0;
-	if (temp)
-		j = ft_strlen(temp);
-	ans = (char *)malloc(sizeof(*temp) * (i + j + 1));
-	ft_memcpy(ans, buff, i);
-	ft_memcpy(ans + i, temp, j);
-	ans[i + j] = '\0';
-	return (ans);
-}
-
-static int      write_in_line(char **buff, char **line, char **temp)
-{
-        int     len;
-        
-	*buff = buff_plus_temp(*buff, *temp);
-        len = find_endl(*buff);
-	if (len != -1)
-        {
-                *line = ft_strdup(*buff);
-                *buff = ft_strdup(*buff + len + 1);
-                return (1);
-        }
-        return (0);
-}
-
-int     get_next_line(int const fd, char **line)
-{
-        static char     *buff[420];
-        int             ret;
-        int             is_okay;
-        char            *temp;
-        temp = ft_strnew(BUFF_SIZE);
-        if (BUFF_SIZE < 0 || fd < 0 || !line || (ret = read(fd, temp, BUFF_SIZE)) <= 0)
-                return (-1);
-        while ((ret = read(fd, temp, BUFF_SIZE)) > 0)
-        {
-                is_okay = write_in_line(&buff[fd], line, &temp);
-                free(temp);
-                if (is_okay == 1)
-                        return (1);
-                temp = ft_strnew(BUFF_SIZE);
-        }
-        if ((is_okay = write_in_line(&buff[fd], line, &temp)))
-                return (1);
-        else if (ft_strlen(buff[fd]) > 0)
-        {
-                *line = ft_strdup(buff[fd]);
-                ft_strdel(&buff[fd]);
-                return (1);
-        }
-        return (is_okay);
+	while ((ret = read(fd, temp, BUFF_SIZE)) > 0)
+		if ((n_found = write_in_line(&buff[fd], line, &temp)) == 1)
+		{
+			ft_memdel((void**)&temp);
+			return (1);
+		}
+	temp = ft_strnew(BUFF_SIZE);
+	if ((n_found = write_in_line(&buff[fd], line, &temp)) == 1)
+	{
+		ft_memdel((void**)&temp);
+		return (1);
+	}
+	else if (ft_strlen(buff[fd]) > 0)
+	{
+		*line = ft_strdup(buff[fd]);
+		ft_memdel((void**)&buff[fd]);
+		return (1);
+	}
+	return (0);
 }
